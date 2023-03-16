8c0cff03157faf74760211ed89a14b4696cb67e3
theophilous7 <theophilous789@gmail.com>
2023-03-16 08:40:53 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ *  main - This is a programme that prints a text using the printf function
+ *  Return: 0 (my program ran successfully)
+ */
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,");
+	Return 0;
+}
