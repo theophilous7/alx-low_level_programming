7b4e659b7ce3208f77a9b256771528fb9a0ec6dd
theophilous7 <theophilous789@gmail.com>
2023-03-17 09:36:34 -0700

    good

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 /**
  * main - A program that prints alphabets in new line
- * return - 0: (success)
+ * Return: 0 (success)
  */
-int main (void)
-{char alphabet[26] = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
+int main(void)
+{char c;
 
-	int i;
-	for (i=0; i= sizeof(alphabet) / sizeof(alphabet[0]; i++
-				{putchar("%c\n"); }
-	return 0;
+	c = 'a';
+
+	while (c <= 'z')
+	{putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
 }
