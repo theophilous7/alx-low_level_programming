f74d67155114a51d129e0fe62724210b709f90c6
theophilous7 <theophilous789@gmail.com>
2023-03-17 08:45:22 -0700

    lsatDigit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,15 +11,15 @@ int main(void)
 {
 	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
 	int last;
 
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 	last = n % 10;
 	if (last > 5)
 	{printf("Last digit of %d is %d and is greater than 5\n", n, last); }
 	if (last == 0)
-	{prinf("Last digit of %d is %d and is 0\n", n, last); }
+	{printf("Last digit of %d is %d and is 0\n", n, last); }
 	if (last < 6 && last != 0)
 	{printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last); }
 	/* your code goes there */
