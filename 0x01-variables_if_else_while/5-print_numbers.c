a37c87bb43c8728994565b3b5b479d8ebb89510d
theophilous7 <theophilous789@gmail.com>
2023-03-18 09:47:04 -0700

    good

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * This is a program that prints nubers from 0 to 9
+ * Return: 0 (success)
+ */
+int main(void)
+{int num;
+
+	num = 2;
+while (num < 10)
+{printf("%d\n", num);
+	num++;
+}
+return (0);
+}
