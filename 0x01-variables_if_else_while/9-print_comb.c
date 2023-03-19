475501bc972f6e86bb33209809b0c515afc0a6fd
theophilous7 <theophilous789@gmail.com>
2023-03-19 08:23:59 -0700

    space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{int num;
+
+	num = 48;
+
+	while (num <= 57)
+	{putchar(num);
+		num++;
+		if (num <= 57)
+			putchar(',');
+	putchar(' ');
+	}
+putchar('\n');
+return (0);
+}
