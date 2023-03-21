9e72c970f94c80b4a53b0dcd786e1d8ea06b1d4f
theophilous7 <theophilous789@gmail.com>
2023-03-21 08:36:49 -0700

    trying

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {putchar('_');
-putchar(p);
-putchar(u);
-putchar(t);
-putchar(c);
-putchar(h);
-putchar(a);
-putchar(r);
-putchar('\n');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar("\n");
 return (0);
 }
