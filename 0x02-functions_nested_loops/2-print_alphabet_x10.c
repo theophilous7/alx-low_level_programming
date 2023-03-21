5e9f14ffc3f8ae446aacfcef1284e20a0d1895cd
theophilous7 <theophilous789@gmail.com>
2023-03-21 10:20:25 -0700

    10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ * Return:void
+ */
+void print_alphabet_x10(void)
+{int i;
+
+int j;
+
+for (i = 0; i <= 10; i++)
+{
+	for (j = 97; j < 123; j++)
+	{putchar(j);
+	}
+putchar('\n');
+}
+}
+
