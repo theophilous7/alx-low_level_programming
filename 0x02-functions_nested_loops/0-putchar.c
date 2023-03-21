2c2ca96e1b11d6343ffa571495e1d57602fbd54a
theophilous7 <theophilous789@gmail.com>
2023-03-21 08:49:46 -0700

    ds

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,13 @@
 int main(void)
 {int p, u, t, c, h, a, r;
 putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
+putchar(p);
+putchar(u);
+putchar(t);
+putchar(c);
+putchar(h);
+putchar(a);
+putchar(r);
 putchar("\n");
 return (0);
 }
