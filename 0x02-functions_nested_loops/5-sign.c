860029d96fb5e7d1dd451f58b9554cebcb5f0ff1
theophilous7 <theophilous789@gmail.com>
2023-03-21 10:39:43 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_sign - print sign
+ * @n: an operand
+ * Return: int
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{_putchar('+');
+	return (1);
+}
+if (n == 0)
+{_putchar('0');
+	return (0);
+}
+if (n < 0)
+{_putchar('-');
+	return (-1);
+}
+}
