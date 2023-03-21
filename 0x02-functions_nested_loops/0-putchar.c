8587be8d28f13a813490c06d93e336e7059252fa
theophilous7 <theophilous789@gmail.com>
2023-03-21 09:03:30 -0700

    sdfa

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,15 +5,14 @@
  * Return: 0 (success)
  */
 int main(void)
-{int p, u, t, c, h, a, r;
-putchar('_');
-putchar(p);
-putchar(u);
-putchar(t);
-putchar(c);
-putchar(h);
-putchar(a);
-putchar(r);
-putchar("\n");
+{_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
