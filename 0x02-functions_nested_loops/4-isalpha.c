7bca4776242c94eaed3ec26854883b38461fb586
theophilous7 <theophilous789@gmail.com>
2023-03-22 04:18:05 -0700

    c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - This is a function that prints c
+ * @c: this is an operand for _isalpha function
+ * Return: 0 (success)
+ */
+int _isalpha(int c)
+{
+if (c == 67 || c == 100)
+	{return (1);
+	}
+return (0);
+}
