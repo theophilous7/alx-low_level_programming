d482882263c64125c2ecfbbf252f7e899a1dee22
theophilous7 <theophilous789@gmail.com>
2023-03-23 05:40:31 -0700

    int

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-/**
- * main - Entry point
- * Return: 0 (success)
- */
-void positive_or_negative(void);
+void positive_or_negative(int i);
 #endif
