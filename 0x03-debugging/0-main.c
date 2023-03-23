d6a20067baa30f76e442be8bf27c95bcd88f1a29
theophilous7 <theophilous789@gmail.com>
2023-03-23 02:59:43 -0700

    debug

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{int i;
+
+i = 98;
+positive_or_negative(i);
+return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+void positive_or_negative(void);
+#endif
