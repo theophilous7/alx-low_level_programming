c54293939c1fbf88d2fd144476ac913a24b02751
theophilous7 <theophilous789@gmail.com>
2023-03-23 08:14:55 -0700

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit - Entry point
+ * @c: only operand
+ * Return: 0 (success)
+ */
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+putchar(48);
+}
+else
+{
+putchar(49);
+}
+}
+
