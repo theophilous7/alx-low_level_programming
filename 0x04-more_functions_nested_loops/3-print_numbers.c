b6b93ae271e50cef46daaa506e65693ce538d675
theophilous7 <theophilous789@gmail.com>
2023-03-24 14:52:51 -0700

    free

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * print_numbers - Entry point
- * Return: void
+ * print_numbers - print numbers
+ * Return: Always 0.
  */
 void print_numbers(void)
 {
-char num[] = "0123456789";
-
 int i;
 
-for (i = 0; i < 10; i++)
+i = 0;
+while (i < 10)
 {
-_putchar(num[i]);
+_putchar (i + '0');
+i++;
 }
+_putchar ('\n');
 }
-
