c2dff1c2a8e1fbdc00230e88baef6f4d2db5bd61
theophilous7 <theophilous789@gmail.com>
2023-04-03 13:11:03 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,15 +9,29 @@ unsigned int _strspn(char *s, char *accept)
 {
 int i, j;
 
-for (i = 0; s[i]; i++)
+int count = 0;
+char *str1, *str2;
+
+str1 = s;
+str2 = accept;
+i = 0;
+while (str1[i] != '\0') /*Declaring WHILE *s */
 {
-for (j = 0; accept[j]; j++)
+j = 0;
+while (str2[j] != '\0') /*Declaring WHILE *accept*/
 {
-if (s[i] == accept[j])
+if (str2[j] == str1[i]) /*Evaluate condition*/
+{
+count++; /*count number*/
 break;
 }
-if (!str2[j])
+j++;    /*add j+1*/
+}
+if (s[i] != accept[j]) /*If aren't equals*/
+{
 break;
 }
-return (i);
+i++; /*add x+1*/
+}
+return (count); /*return the value of count*/
 }
