ae9a1667519aa59fad107ec0edc226bb355b96e7
theophilous7 <theophilous789@gmail.com>
2023-04-05 07:21:32 -0700

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _pow_recursion - power recursion
+ * @x: first operand
+ * @y: second operand
+ * Return: integer
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+if (y == 1)
+{
+return (x);
+}
+else
+{
+return (x * _pow_recursion(x, y - 1));
+}
+}
