70097ddf957f9c74533db8b6234f8f3337db8930
theophilous7 <theophilous789@gmail.com>
2023-04-05 04:03:55 -0700

    putsr

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 /**
  * _puts_recursion - a function that prints a set of characters
  * @s: only operand
@@ -11,6 +12,10 @@ int i;
 i = 0;
 _putchar(s[i]);
 s++;
+if (s[i] == NULL)
+{
+return;
+}
 if (s[i] != '\0')
 {
 _puts_recursion((char *)s);
