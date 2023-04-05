d5ebaad7f350ae81d064ec537ae3efb3275d8ea3
theophilous7 <theophilous789@gmail.com>
2023-04-05 03:34:51 -0700

    putsRecursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - a function that prints a set of characters
+ * @s: only operand
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+int i;
+
+i = 0;
+_putchar(s[i]);
+*s++;
+if (s[i] != '\0')
+{
+_puts_recursion((char *)s);
+}
+}
