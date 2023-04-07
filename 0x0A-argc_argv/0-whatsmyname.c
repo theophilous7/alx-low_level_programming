404ea6e976f510c7223acab7212bdcbfe06970c7
theophilous7 <theophilous789@gmail.com>
2023-04-07 10:55:46 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point of program
+ * @argc: first operand
+ * @argv: second operand
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+}
