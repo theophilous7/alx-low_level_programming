27ca2fdcce3f0250a7b649665aa36ac78f3d1dd2
theophilous7 <theophilous789@gmail.com>
2023-04-09 14:20:03 -0700

    multiplying arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: frist operand
+ * @argv: second operand
+ * Return: integer (success)
+ */
+int main(int argc, char *argv[])
+{
+int first, second, result;
+
+if (argc < 2)
+{
+printf("Error\n");
+return (1);
+}
+first = atoi(argv[1]);
+second = atoi(argv[2]);
+result = first *second;
+printf("%d\n", result);
+return (0);
+}
