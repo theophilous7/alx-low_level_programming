a9a598f6672b73d399820a710067e60b8054769b
theophilous7 <theophilous789@gmail.com>
2023-04-09 13:09:38 -0700

    agrg count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: first operand
+ * @argv: second operand
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc);
+return (0);
+}
