26fd783888f5bd1c80306a17f418de1c253a6527
theophilous7 <theophilous789@gmail.com>
2023-04-09 14:28:51 -0700

    multiplyng of arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,14 +8,16 @@
  */
 int main(int argc, char *argv[])
 {
-int first, second;
+int first, second, result;
 
-if (argc < 2)
+if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
 first = atoi(argv[1]);
 second = atoi(argv[2]);
-return (first *second);
+result = first *second;
+printf("%d\n", result);
+return (0);
 }
