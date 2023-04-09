071256130c1e14fea187eca434933ac27ec65af4
theophilous7 <theophilous789@gmail.com>
2023-04-09 12:52:53 -0700

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,8 @@
  * @argv: second operand
  * Return: 0 (success)
  */
-int main(int __attribute__ ((unused)) argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 printf("%s\n", argv[0]);
-printf("\n");
 return (0);
 }
