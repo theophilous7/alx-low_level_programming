802a9d004b74dbf7058407b90b27ce362197070a
theophilous7 <theophilous789@gmail.com>
2023-04-17 11:31:45 -0700

    struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog - entry point
+ * @d: first
+ * @name: second 
+ * @age: third
+ * @owner: fourth
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+struct dog cheetah;
+}
+
