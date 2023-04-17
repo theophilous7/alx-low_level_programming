ef73c170b707096e38a82339f6707a4570f7b4dc
theophilous7 <theophilous789@gmail.com>
2023-04-17 08:09:37 -0700

    main

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,7 +3,7 @@
  * this a program that prints its name using command line argument
  * Return: void
  */
-int main(int argc __attribute__((unused)), char *arg[])
+int main(int argc __attribute__((unused)), char *argv[])
 {       
 printf("%s\n", argv[0]);
 return (0);
