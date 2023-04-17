7521650b78a21cf1cef3aa33491c28be11c4ee8a
theophilous7 <theophilous789@gmail.com>
2023-04-17 10:45:21 -0700

    struct of dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,16 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - entry point
+ * description - this is a header that defines the struct dog
+ * @name: first property
+ * @age: second property
+ * @owner: third property
+ */
 struct dog
 {
 char *name;
 float age;
 char *owner;
 }
+#endif
