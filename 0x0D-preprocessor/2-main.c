8379a6793c8538f1f7970f354c40cc2949b493b6
theophilous7 <theophilous789@gmail.com>
2023-04-17 08:20:12 -0700

    main

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
- * this a program that prints its name using command line argument
- * Return: void
+ * main - entry point of program
+ * @argc: first operand
+ * @argv: second operand
+ * Return: 0 (success)
  */
 int main(int argc __attribute__((unused)), char *argv[])
-{       
+{
 printf("%s\n", argv[0]);
 return (0);
 }
