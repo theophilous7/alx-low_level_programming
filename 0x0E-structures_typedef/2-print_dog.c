a0babb625a48dba554a8965e9da57b27192c85f8
theophilous7 <theophilous789@gmail.com>
2023-04-18 15:33:44 -0700

    print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,14 +11,18 @@ void print_dog(struct dog *d)
 {
 if (d == NULL)
 {
+d = malloc(sizeof(struct dog));
 printf("nothing\n");
 }
 if (d->name == NULL)
 {
+free(d);
 printf("Name: (nil)\n");
 }
 if (d->owner == NULL)
 {
+free(d->name);
+free(d);
 printf("Owner: (nil)\n");
 }
 printf("Name: %s\n", d->name);
