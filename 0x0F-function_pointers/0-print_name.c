106fa9a57fb9797b6807aa2f82cb4b744c6cf1e0
theophilous7 <theophilous789@gmail.com>
2023-04-19 10:53:17 -0700

    print

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 /**
  * print_name - entry point
  * @name: first operand
@@ -8,7 +7,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-if (f ==  NULL)
+if (f && name)
 {
 return;
 }
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,25 @@
+#include "3-calc.h"
+/**
+ * @a: first operand
+ * @b: second operand
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+int op_div(int a, int b)
+{
+return (a / b);
+}
+int op_div(int a, int b)
+{
+return (a % b);
+}
