619ccb2be9d261edc1052abe34657ddadb71e528
theophilous7 <theophilous789@gmail.com>
2023-04-19 07:50:12 -0700

    index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+/**
+ * int_index - entry point
+ * @array: first
+ * @size: second
+ * @cmp: third
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+
+if (size <= 0)
+{
+return (-1);
+}
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) == array[i])
+{
+return (i);
+}
+else
+{
+return (-1);
+}
+}
+return (0);
+}
