5e771d544dcaad2ea98b6238c10fac1f82951a73
theophilous7 <theophilous789@gmail.com>
2023-04-19 04:47:23 -0700

    print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - entry point
+ * @name: first operand
+ * @f: second operand
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
