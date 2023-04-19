dc011c9eb2781ef79083458dc24fe276b591421a
theophilous7 <theophilous789@gmail.com>
2023-04-19 07:09:18 -0700

    array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * array_iterator - entry point
+ * @array: first
+ * @size: second
+ * @action: third
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+
+if (action == NULL)
+{
+return;
+}
+for (i = 0; i <= size; i++)
+{
+action(array[size]);
+}
+}
